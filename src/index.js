c3d00937c92440130a2f6eece43c0f625dbdf27b
Jan Monterrubio <janmonterrubio@gmail.com>
2019-12-15 22:15:57 -0600

    gather trend data

--- src/index.js
+++ src/index.js
@@ -11,6 +11,48 @@ import { gatherStats } from "./trends/trendsAPI";
 
 require("dotenv").config();
 
+/**
+ * Gathers trend data across seasons and lifetime, returning an object in the form:
+ {
+  "seasonal": [
+    {
+      "division.bro.official.pc-2018-01": {
+        "duo-fpp": {
+          "kd": "0.78",
+          "winRate": "3.81"
+        },
+        "solo-fpp": {
+          "kd": "1.12",
+          "winRate": "0.00"
+        },
+        "squad-fpp": {
+          "kd": "0.85",
+          "winRate": "2.53"
+        }
+      }
+    }...
+  ],
+  "lifetime": {
+    "duo-fpp": {
+      "kd": "0.91",
+      "winRate": "3.94"
+    },
+    "solo-fpp": {
+      "kd": "1.07",
+      "winRate": "0.00"
+    },
+    "squad": {
+      "kd": "2.00",
+      "winRate": "0.00"
+    },
+    "squad-fpp": {
+      "kd": "0.93",
+      "winRate": "4.97"
+    }
+  }
+}
+ * @param {String} playerName 
+ */
 async function gatherTrend(playerName) {
   const history = await getHistory(playerName);
   const playerId = PlayerCache.getId(playerName);
@@ -22,7 +64,7 @@ async function gatherTrend(playerName) {
   for (const seasonId of seasonEntries) {
     const seasonEntry = history[seasonId];
     const seasonStats = gatherStats(seasonEntry.data);
-    seasonalStats.push({ seasonId: seasonStats });
+    seasonalStats.push({ [seasonId]: seasonStats });
   }
 
   const lifetimeInfo = gatherStats(lifetime.data);
