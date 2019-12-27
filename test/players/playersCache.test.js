6e2a8d9f9844ee6f6287b918897c89269a8709c6
AnEmortalKid <janmonterrubio@gmail.com>
2019-12-27 15:07:24 -0600

    assert storage

--- test/players/playersCache.test.js
+++ test/players/playersCache.test.js
@@ -1,25 +1,37 @@
 import { getCache } from "../../src/players/playerCache";
 
-const mockStorageFind = jest.fn();
+const mockFind = jest.fn();
+const mockStore = jest.fn();
 const mockStorage = {
-  find: mockStorageFind
+  find: mockFind,
+  store: mockStore
 };
 
 const playerCache = getCache(mockStorage);
 
-describe("storeId", () => {
+describe("getId", () => {
   it("returns null when player not found", () => {
-    mockStorageFind.mockReturnValueOnce(null);
+    mockFind.mockReturnValueOnce(null);
     const foundId = playerCache.getId("someName");
     expect(foundId).toBeNull();
   });
 
   it("returns playerId when stored in cache", () => {
-    mockStorageFind.mockReturnValueOnce({
+    mockFind.mockReturnValueOnce({
       id: "account.someValue",
       name: "someName"
     });
     const foundId = playerCache.getId("someName");
     expect(foundId).toBe("account.someValue");
+  });
+});
+
+describe("storeId", () => {
+  it("stores the player on the storage", () => {
+    playerCache.storeId("account.someId", "playerName");
+    expect(mockStore).toHaveBeenCalledWith("players", {
+      id: "account.someId",
+      name: "playerName"
+    });
   });
 });
