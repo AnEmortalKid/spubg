e8e61615c0b64ca7989c47fd63940a7c55c38dcd
Jan Monterrubio <janmonterrubio@gmail.com>
2019-12-31 00:07:49 -0600

    wip extract trend chart

---
+++ src/chart/trendChart.js
@@ -0,0 +1,15 @@
+import BaseChart from "./baseChart";
+
+export class TrendChart extends BaseChart
+{
+    constructor(chartOptions)
+    {
+        super("trend")
+        this.chartOptions = chartOptions;
+    }
+
+    create()
+    {
+
+    }
+}
