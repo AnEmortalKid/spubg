32167bf8375aa2b117cbf686aef05581d6b936bd
Jan Monterrubio <janmonterrubio@gmail.com>
2019-12-31 18:10:18 -0600

    history api test

--- test/history/historyAPI.test.js
+++ test/history/historyAPI.test.js
@@ -30,11 +30,16 @@ describe("get", () => {
     // setup player id
     mockPlayers.findId.mockReturnValue("someId");
     // setup seasons
-    mockSeasons.getSearchableIds.mockReturnValue(["seasonOneId", "seasonTwoId"]);
+    mockSeasons.getSearchableIds.mockReturnValue([
+      "seasonOneId",
+      "seasonTwoId"
+    ]);
     // nothing stored
     mockCache.get.mockReturnValue(null);
     // setup client to return seasonOneData -> season2Data
-    mockClient.playerSeason.mockReturnValue("seasonTwoData").mockReturnValueOnce("seasonOneData");
+    mockClient.playerSeason
+      .mockReturnValue("seasonTwoData")
+      .mockReturnValueOnce("seasonOneData");
 
     const somePlayerHist = await history.get("somePlayer");
 
@@ -47,6 +52,59 @@ describe("get", () => {
     expect(mockCache.store).toHaveBeenCalledWith("someId", {
       seasonOneId: "seasonOneData",
       seasonTwoId: "seasonTwoData"
+    });
+  });
+
+  it("updates info for the latest season when history exists", async () => {
+    // setup player id
+    mockPlayers.findId.mockReturnValue("someId");
+    // setup seasons
+    mockSeasons.getSearchableIds.mockReturnValue(["latestSeason"]);
+    mockSeasons.getLatestSeasonId.mockReturnValue("latestSeason");
+    // setup cache
+    mockCache.get.mockReturnValue({
+      latestSeason: "staleSeasonData"
+    });
+    // setup client to return new data for the latest season
+    mockClient.playerSeason.mockReturnValue("newSeasonData");
+
+    const somePlayerHist = await history.get("somePlayer");
+
+    expect(somePlayerHist).toEqual({
+      latestSeason: "newSeasonData"
+    });
+
+    // it should store the updated data
+    expect(mockCache.store).toHaveBeenCalledWith("someId", {
+      latestSeason: "newSeasonData"
+    });
+  });
+
+  it("finds older season data if the history did not store it", async () => {
+    // setup player id
+    mockPlayers.findId.mockReturnValue("someId");
+    // setup seasons
+    mockSeasons.getSearchableIds.mockReturnValue(["latestSeason", "pastSeason"]);
+    mockSeasons.getLatestSeasonId.mockReturnValue("latestSeason");
+    // setup cache
+    mockCache.get.mockReturnValue({
+      latestSeason: "staleSeasonData"
+    });
+    // setup client to return new data for the latest season
+    // newSeasonData -> pastSeasonData
+    mockClient.playerSeason.mockReturnValue("pastSeasonData").mockReturnValueOnce("newSeasonData");
+
+    const somePlayerHist = await history.get("somePlayer");
+
+    expect(somePlayerHist).toEqual({
+      latestSeason: "newSeasonData",
+      pastSeason: "pastSeasonData"
+    });
+
+    // it should store the updated data
+    expect(mockCache.store).toHaveBeenCalledWith("someId", {
+      latestSeason: "newSeasonData",
+      pastSeason: "pastSeasonData"
     });
   });
 });
